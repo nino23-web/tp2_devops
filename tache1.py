acf4108694364dfed77539fc902831a104ea324f
nino23-web <tineninoarmand23@gmail.com>
2024-10-18 17:07:40 +0200

    Update tache1.py

--- tache1.py
+++ tache1.py
@@ -1,9 +1,8 @@
-from tache2 import Game
-from tache3 import setup_gui
-
-def main():
-    game = Game()
-    setup_gui(game)
-
-if _name_ == "_main_":
-    main()
+def obtenir_questions():
+    return [
+        {"question": "Quel est le plus grand pays du monde ?", "reponse": "Russie"},
+        {"question": "Quelle est la capitale de la France ?", "reponse": "Paris"},
+        {"question": "Combien de continents y a-t-il ?", "reponse": "7"},
+        {"question": "Qui a peint la Joconde ?", "reponse": "Léonard de Vinci"},
+        {"question": "Quel est l'animal le plus rapide du monde ?", "reponse": "Guépard"}
+    ]
